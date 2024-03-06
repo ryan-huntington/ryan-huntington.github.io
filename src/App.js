b1a9600785a7c4992b746d5e56b9b9c5fd2b7ec9
Ryan Huntington <ryanhuntington12@gmail.com>
2024-03-05 18:32:32 -0600

    Made navbar and basic routing structure. Basic pages implemented

--- src/App.js
+++ src/App.js
@@ -1,8 +1,47 @@
-function App(){
-    return(
-        <div>
-            
-        </div>
-    )
+import { Route, Routes, Navigate } from 'react-router-dom';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import AboutMe from './pages/AboutMe';
+import Navbar from './Navbar';
+import React, { useState } from "react";
+import {FaHome, FaInbox, FaUser} from "react-icons/fa"
+
+function App() {
+    const [pages, setPages] = useState([{
+        icon: <FaHome />,
+        text: 'Home',
+        to: "/"
+      }, {
+        icon: <FaInbox />,
+        text: 'Projects',
+        to: "/projects"
+      }, {
+        icon: <FaUser />,
+        text: 'About Me',
+        to: "/about"
+      }])
+    function changeActiveNavItem(item){
+        const newItems = pages.map((page, index) => ({
+            ...page,
+            active: index === item
+        }));
+        newItems[item].active = true;
+    
+        setPages(newItems)
+
+    }
+  return (
+    <>
+        <Navbar items={pages} changeActiveNavItem={changeActiveNavItem} />
+        <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/about" element={<AboutMe />} />
+            <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+    </>
+      
+  );
 }
-export default App
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome</h1>
+      <p>My name is Ryan Huntington. I'm a software engineer with skills in building applications
+        and deep learning. Currently, I'm working on completing my Bachelors of Science at Baylor University. 
+      </p>
+      <div className='projects-button'>
+        <Link to="/projects">Explore my Projects!</Link> 
+      </div>
+    </div>
+  );
+}
+
+export default Home;
