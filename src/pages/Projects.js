f2a3fa702deb52a4d584438c0ae09504a2ef6e2b
Ryan Huntington <ryanhuntington12@gmail.com>
2024-03-09 21:01:35 -0600

    updated readme and small update to projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import projectsData from './projectsData.json'
 import './styles.css'
 
 function Projects() {
+  const [filteredProjects, setFilteredProjects] = useState(projectsData)
+  const [selectedTags, setSelectedTags] = useState([]);
+  const [selectedExperienceLevel, setSelectedExperienceLevel] = useState('');
+  const [selectedProjectType, setSelectedProjectType] = useState('');
+
+  const handleTagFilter = (tag) => {
+    // Logic to filter projects based on selected tags
+    setSelectedTags([...selectedTags, tag]);
+    const filtered = projectsData.filter(project =>
+      project.tags.includes(tag)
+    );
+    setFilteredProjects(filtered);
+  };
+
+  const handleExperienceLevelFilter = (level) => {
+    // Logic to filter projects based on selected experience level
+    setSelectedExperienceLevel(level);
+    const filtered = projectsData.filter(project =>
+      project.experienceLevel === level
+    );
+    setFilteredProjects(filtered);
+  };
+
+  const handleProjectTypeFilter = (type) => {
+    // Logic to filter projects based on selected project type
+    setSelectedProjectType(type);
+    const filtered = projectsData.filter(project =>
+      project.projectType === type
+    );
+    setFilteredProjects(filtered);
+  };
   return (
     <div className='page'>
       <h1>Projects</h1>
-      <p>Here you can find information about my projects.</p>
+      {filteredProjects.map(project => (
+        <p>{project.name}</p>
+      ))}
     </div>
   );
 }
