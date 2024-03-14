1a4d21c46a3c38dfc8439e0df6794b93244bad65
Ryan Huntington <ryan_huntington1@gmail.com>
2024-03-14 16:40:24 -0500

    Projects page updated

--- src/components/FilterDropdown.js
+++ src/components/FilterDropdown.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './Projects.css'
 import Select from "react-select"
 
--- src/components/ProjectSquare.js
+++ src/components/ProjectSquare.js
@@ -4,14 +4,17 @@ import './Projects.css'
 const ProjectSquare = ({ project }) => {
     return (
         <div className="project-square">
+            <div className='content'>
             <h3>{project.name}</h3>
             <p>{project.description}</p>
-            <p>Tags: {project.tags.join(', ')}</p>
+            <p>Languages: {project.tags.join(', ')}</p>
             <p>Experience Level: {project.experienceLevel}</p>
             <p>Project Type: {project.projectType}</p>
             <a href={project.link} target="_blank"
                 rel="noreferrer"
             >Learn More</a>
+            </div>
+            
         </div>
     );
 };
